e11bc78b217ec292f571cca552282904504ecd04
Danang Galuh Tegar Prasetyo <connect@danang.id>
2020-06-09 01:37:06 +0700

    initial project

---
+++ src/components/PortfolioBox.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function PortfolioBox() {
+	return (
+		<div className="portfolio-box">
+			Portfolio Box
+		</div>
+	)
+}
+
+export default PortfolioBox
---
+++ src/components/VisionBox.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function VisionBox() {
+	return (
+		<div className="vision-box">
+			Vision Box
+		</div>
+	)
+}
+
+export default VisionBox
